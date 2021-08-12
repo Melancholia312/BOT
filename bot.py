19b9a5691fad2694ea1d50a7923c505c537852df
Melancholia312 <67428510+Melancholia312@users.noreply.github.com>
2021-08-12 19:48:53 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1125,25 +1125,26 @@ def index(msg, user_id, peer_id):
                                                   "- https://vk.com/club203434371")
 
     elif is_exists(user_id):
+        
         if get_user_flag(user_id)['flag'] != 5:
             user_register(msg, user_id)
 
         elif check_dange_floor(user_id) > 0:
             dange_gameplay(msg, user_id, peer_id)
+           
+        elif clear_msg(msg, 'помощь'):
+                space = "\n" + '~~~~~~~~~~~~~' + "\n"
+                answer = "Полезные ссылки" + space + '📚Краткое руководство:' + '\n' +  'https://vk.com/topic-203434371_48149392' + '\n' + '🎯Советы для новых игроков:' + '\n' + 'https://vk.com/topic-203434371_48174280' + '\n' + \
+                         '☎Исправление багов:' + '\n' +  'https://vk.com/topic-203434371_47471775' + \
+                         '\n' + '\n' + 'По важным вопросам писать:' + '\n' + '🍰 [id276221064|@melancholia312]' + space
+                send_message(peer_id=peer_id, text=answer, keyboard=create_keyboard(text=['/меню', '/помощь'], inline=False))
 
         elif '/' in msg:
 
             if clear_msg(msg, 'точно удалить персонажа'):
                 delete_hero(user_id)
                 answer = "Да уж!"
-                send_message(peer_id=peer_id, text=answer)
-
-            elif clear_msg(msg, 'помощь'):
-                space = "\n" + '~~~~~~~~~~~~~' + "\n"
-                answer = "Полезные ссылки" + space + '📚Краткое руководство:' + '\n' +  'https://vk.com/topic-203434371_48149392' + '\n' + '🎯Советы для новых игроков:' + '\n' + 'https://vk.com/topic-203434371_48174280' + '\n' + \
-                         '☎Исправление багов:' + '\n' +  'https://vk.com/topic-203434371_47471775' + \
-                         '\n' + '\n' + 'По важным вопросам писать:' + '\n' + '🍰 [id276221064|@melancholia312]' + space
-                send_message(peer_id=peer_id, text=answer, keyboard=create_keyboard(text=['/меню', '/помощь'], inline=False))
+                send_message(peer_id=peer_id, text=answer)       
 
             elif clear_msg(msg, 'меню'):
                 text_for_buttons = ['/мой персонаж', '/магазин', '/аукцион',
